4b65e26be1c072d02e7e8a6a2bd25fafa4f54c50
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-10-28 01:52:19 +0300

    feat: added `groups` option for `classToPlain` in response interceptor (auth)

--- packages/crud/src/interceptors/crud-response.interceptor.ts
+++ packages/crud/src/interceptors/crud-response.interceptor.ts
@@ -4,8 +4,12 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { isFalse, isObject, isFunction } from '@dataui/crud-util';
-import { classToPlain, classToPlainFromExist } from 'class-transformer';
+import { isFalse, isObject, isFunction } from '@nestjsx/util';
+import {
+  classToPlain,
+  classToPlainFromExist,
+  ClassTransformOptions,
+} from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CrudActions } from '../enums';
@@ -27,43 +31,56 @@ const actionToDtoNameMap: {
 };
 
 @Injectable()
-export class CrudResponseInterceptor extends CrudBaseInterceptor
-  implements NestInterceptor {
+export class CrudResponseInterceptor
+  extends CrudBaseInterceptor
+  implements NestInterceptor
+{
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(map((data) => this.serialize(context, data)));
   }
 
-  protected transform(dto: any, data: any) {
+  protected transform(dto: any, data: any, options: ClassTransformOptions) {
     if (!isObject(data) || isFalse(dto)) {
       return data;
     }
 
     if (!isFunction(dto)) {
-      return data.constructor !== Object ? classToPlain(data) : data;
+      return data.constructor !== Object ? classToPlain(data, options) : data;
     }
 
     return data instanceof dto
-      ? classToPlain(data)
-      : classToPlain(classToPlainFromExist(data, new dto()));
+      ? classToPlain(data, options)
+      : classToPlain(classToPlainFromExist(data, new dto()), options);
   }
 
   protected serialize(context: ExecutionContext, data: any): any {
+    const req = context.switchToHttp().getRequest();
     const { crudOptions, action } = this.getCrudInfo(context);
     const { serialize } = crudOptions;
     const dto = serialize[actionToDtoNameMap[action]];
     const isArray = Array.isArray(data);
 
+    const options: ClassTransformOptions = {};
+    /* istanbul ignore else */
+    if (isFunction(crudOptions.auth?.groups)) {
+      const userOrRequest = crudOptions.auth.property
+        ? req[crudOptions.auth.property]
+        : req;
+
+      options.groups = crudOptions.auth.groups(userOrRequest);
+    }
+
     switch (action) {
       case CrudActions.ReadAll:
         return isArray
-          ? (data as any[]).map((item) => this.transform(serialize.get, item))
-          : this.transform(dto, data);
+          ? (data as any[]).map((item) => this.transform(serialize.get, item, options))
+          : this.transform(dto, data, options);
       case CrudActions.CreateMany:
         return isArray
-          ? (data as any[]).map((item) => this.transform(dto, item))
-          : this.transform(dto, data);
+          ? (data as any[]).map((item) => this.transform(dto, item, options))
+          : this.transform(dto, data, options);
       default:
-        return this.transform(dto, data);
+        return this.transform(dto, data, options);
     }
   }
 }
